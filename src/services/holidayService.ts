81506d81f664a7a6abb99bcd71bd25319557f2ae
Denis Chornyi <chornyi.den@gmail.com>
2025-01-09 12:26:38 +0200

    fix getHolidays

--- src/services/holidayService.ts
+++ src/services/holidayService.ts
@@ -2,19 +2,6 @@ import { Holiday } from "../types";
 
 const BASE_URL = "https://date.nager.at/api/v3";
 
-async function getCountryCode(): Promise<string> {
-  try {
-    const response = await fetch("https://ipapi.co/json/");
-    if (!response.ok) throw new Error("Failed to fetch country code");
-    const data = await response.json();
-    return data.country_code || "US";
-  } catch (error) {
-    console.error("Error fetching country code:", error);
-
-    return getCountryByTimeZone();
-  }
-}
-
 function getCountryByTimeZone(): string {
   const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
   const timeZoneMapping: { [key: string]: string } = {
@@ -25,12 +12,12 @@ function getCountryByTimeZone(): string {
     "Europe/Berlin": "DE",
   };
 
-  return timeZoneMapping[timeZone] || "US";
+  return timeZoneMapping[timeZone] || "UA";
 }
 
 export async function getHolidays(year: number): Promise<Holiday[]> {
   try {
-    const countryCode = await getCountryCode();
+    const countryCode = await getCountryByTimeZone();
     const response = await fetch(
       `${BASE_URL}/PublicHolidays/${year}/${countryCode}`
     );
